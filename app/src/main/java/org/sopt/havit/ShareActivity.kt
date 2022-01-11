51010d6af3601cf801f5dff66652a301e1fcd5dd
KxxHyoRim <rlagyfla0709@gmail.com>
2022-01-12 05:35:51 +0900

    [UI/FEAT] Set Base Bottom Sheet on ShareActivity (#7)

--- app/src/main/java/org/sopt/havit/ShareActivity.kt
+++ app/src/main/java/org/sopt/havit/ShareActivity.kt
@@ -2,9 +2,8 @@ package org.sopt.havit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import org.sopt.havit.databinding.ActivityShareBinding
+import org.sopt.havit.ui.share.BottomSheetShareFragment
 
 class ShareActivity : AppCompatActivity() {
 
@@ -16,10 +15,8 @@ class ShareActivity : AppCompatActivity() {
         binding = ActivityShareBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_share, null)
-        val bottomSheetDialog = BottomSheetDialog(this)
-        bottomSheetDialog.setContentView(bottomSheetView)
-        bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
-        bottomSheetDialog.show()
+        val bottomSheet = BottomSheetShareFragment()
+        bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+
     }
 }
