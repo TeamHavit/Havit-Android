2414c71c57678ac91ce9e43990c13d85868be8be
KxxHyoRim <rlagyfla0709@gmail.com>
2022-01-13 04:47:28 +0900

    [UI] Add ChooseIconFragment (#7)

---
+++ app/src/main/java/org/sopt/havit/ui/share/ChooseIconFragment.kt
@@ -0,0 +1,20 @@
+package org.sopt.havit.ui.share
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.sopt.havit.R
+
+class ChooseIconFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_icon, container, false)
+    }
+
+}
