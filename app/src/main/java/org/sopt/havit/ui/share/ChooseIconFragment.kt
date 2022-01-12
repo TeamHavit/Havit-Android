703a4198742bc7bbbd9dfc1577ea37db4167958f
KxxHyoRim <rlagyfla0709@gmail.com>
2022-01-13 05:26:59 +0900

    [FEAT] Add toast_category_added.xml (#7)

--- app/src/main/java/org/sopt/havit/ui/share/ChooseIconFragment.kt
+++ app/src/main/java/org/sopt/havit/ui/share/ChooseIconFragment.kt
@@ -5,16 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import org.sopt.havit.R
+import org.sopt.havit.databinding.FragmentChooseIconBinding
+import org.sopt.havit.databinding.FragmentNoCategoryBinding
 
 class ChooseIconFragment : Fragment() {
+    private var _binding: FragmentChooseIconBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_icon, container, false)
+        _binding = FragmentChooseIconBinding.inflate(layoutInflater, container, false)
+
+        binding.btnNext.setOnClickListener {
+
+            val toast = Toast(requireContext())
+            // set text
+            val view = layoutInflater.inflate(R.layout.toast_category_added, null)
+            val textView : TextView = view.findViewById(R.id.tv_toast_category)
+            textView.text = "커스텀 토스트 메세지"
+            toast.view = view
+            toast.show()
+
+        }
+
+        return binding.root
     }
 
 }
