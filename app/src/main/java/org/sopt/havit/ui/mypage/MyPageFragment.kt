0f8124426e5f2c44aa2140da9f1411780d90435c
안나영 <dpswpfskdud@naver.com>
2022-01-14 13:42:02 +0900

    [FEAT] CustomWebview havit feat (#43)

--- app/src/main/java/org/sopt/havit/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/org/sopt/havit/ui/mypage/MyPageFragment.kt
@@ -5,15 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.sopt.havit.R
 import org.sopt.havit.databinding.FragmentMyPageBinding
+import org.sopt.havit.ui.contents.ContentsViewModel
 import org.sopt.havit.util.web.CustomWebView
+import org.sopt.havit.util.web.DigBroadcastReceiver
+import kotlin.coroutines.coroutineContext
 
 class MyPageFragment : Fragment() {
 
-    private val myPageViewModel: MyPageViewModel by viewModel()
+    //private val myPageViewModel: MyPageViewModel by viewModel()
+     val contentsViewModel:ContentsViewModel by viewModels()
     private var _binding: FragmentMyPageBinding? = null
 
     // This property is only valid between onCreateView and
@@ -27,19 +32,22 @@ class MyPageFragment : Fragment() {
     ): View? {
 
         _binding = FragmentMyPageBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+
 
         binding.textHome.setOnClickListener {
-            /*CustomWebView.setView(
+            CustomWebView.setView(
                 requireContext(),
                 "https://github.com/boostcampwm-2021/android03-Contact/wiki/Git-%EC%82%AC%EC%9A%A9%EB%B2%95"
-            )*/
-            findNavController().navigate(R.id.action_navigation_my_page_to_searchFragment)
-
+            )
+            //findNavController().navigate(R.id.action_navigation_my_page_to_searchFragment)
         }
 
-        return root
+
+
+
+        return binding.root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
