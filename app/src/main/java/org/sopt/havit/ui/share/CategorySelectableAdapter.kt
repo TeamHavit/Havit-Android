411315a08a5639b8f3e33764b7047723a58bfc3b
KxxHyoRim <rlagyfla0709@gmail.com>
2022-01-15 07:22:27 +0900

    [FEAT/UI] Add SelectCategoryFragment (#9)

---
+++ app/src/main/java/org/sopt/havit/ui/share/CategorySelectableAdapter.kt
@@ -0,0 +1,37 @@
+package org.sopt.havit.ui.share
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.havit.R
+import org.sopt.havit.data.CategorySelectableData
+import org.sopt.havit.databinding.ItemCategorySelectableBinding
+
+class CategorySelectableAdapter :
+    RecyclerView.Adapter<CategorySelectableAdapter.CategorySelectableViewHolder>() {
+    val categorySelectableList = mutableListOf<CategorySelectableData>()
+
+    override fun onCreateViewHolder( parent: ViewGroup, viewType: Int ): CategorySelectableViewHolder {
+        val binding: ItemCategorySelectableBinding =
+            DataBindingUtil.inflate( LayoutInflater.from(parent.context),
+                R.layout.item_category_selectable,
+                parent,
+                false )
+
+        return CategorySelectableViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CategorySelectableViewHolder, position: Int) {
+        holder.onBind(categorySelectableList[position])
+    }
+
+    override fun getItemCount(): Int = categorySelectableList.size
+
+    class CategorySelectableViewHolder(private val binding: ItemCategorySelectableBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: CategorySelectableData) {
+            binding.selectableCategory = data
+        }
+    }
+}
