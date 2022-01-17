0a080dd8e2288101c7b0f9d632396f6dce7f3cb3
KxxHyoRim <rlagyfla0709@gmail.com>
2022-01-18 02:03:35 +0900

    [FEAT] Add Function getting UserDeviceToken (#88)

--- app/src/main/java/org/sopt/havit/notification/MyFirebaseMessagingService.kt
+++ app/src/main/java/org/sopt/havit/notification/MyFirebaseMessagingService.kt
@@ -11,6 +11,8 @@ import android.util.Log
 import android.widget.RemoteViews
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import org.sopt.havit.MainActivity
@@ -18,6 +20,7 @@ import org.sopt.havit.R
 
 const val channelID = "notification_channel"
 const val channelName = "org.sopt.androidsharing"
+
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
@@ -26,9 +29,28 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         Log.d("MyFirebaseMessagingService", "Refreshed token: $token")
     }
 
+    private fun getDeviceToken() {
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.d("TokenTest", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+            Log.d("TokenTest", token)
+
+        })
+    }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d("MyFirebaseMessagingService", "onMessageReceived")
+        getDeviceToken()
+
+        remoteMessage.data.let {
+            Log.d("MyFirebaseMessagingService", "Message data payload: ${remoteMessage.data}")
+        }
 
         // Check if message contains a notification payload.
         remoteMessage.notification?.let {
@@ -37,7 +59,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         }
     }
 
-    private fun getRemoteView(title:String, message:String) : RemoteViews {
+    private fun getRemoteView(title: String, message: String): RemoteViews {
         val remoteView = RemoteViews("com.sopt.androidsharing", R.layout.push_notification)
         remoteView.setTextViewText(R.id.tv_title, title)
         remoteView.setTextViewText(R.id.tv_description, message)
@@ -55,11 +77,11 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
 
         var builder = NotificationCompat.Builder(this, channelID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setAutoCancel(true)
-                .setVibrate(longArrayOf(1000, 500, 1000, 500))  // 1초 울리고 0.5초 쉬고
-                .setOnlyAlertOnce(true)
-                .setContentIntent(pendingIntent)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000, 500, 1000, 500))  // 1초 울리고 0.5초 쉬고
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
 
         builder = builder.setContent(getRemoteView(title, message))    // custom
 
@@ -67,9 +89,10 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         /** Oreo Version 이하일때 처리 하는 코드 */
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             Log.d("MyFirebaseMessagingService", "under Oreo Version")
-            val notificationChannel = NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH)
+            val notificationChannel =
+                NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH)
             notificationManager.createNotificationChannel(notificationChannel)
         }
 
