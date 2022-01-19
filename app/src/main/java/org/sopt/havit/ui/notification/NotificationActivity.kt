3c5da57e026e5ad6961482709dc075e7e4a02889
yubinquitous <yb9854@naver.com>
2022-01-20 00:50:55 +0900

    [FEAT] Set BackBtn clickEvent in NotificationActivity (#134)

--- app/src/main/java/org/sopt/havit/ui/notification/NotificationActivity.kt
+++ app/src/main/java/org/sopt/havit/ui/notification/NotificationActivity.kt
@@ -1,12 +1,22 @@
 package org.sopt.havit.ui.notification
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.havit.R
+import org.sopt.havit.databinding.ActivityNotificationBinding
+import org.sopt.havit.ui.base.BaseBindingActivity
 
-class NotificationActivity : AppCompatActivity() {
+class NotificationActivity :
+    BaseBindingActivity<ActivityNotificationBinding>(R.layout.activity_notification) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notification)
+        setContentView(binding.root)
+
+        clickBackBtn()
+    }
+
+    private fun clickBackBtn() {
+        binding.ivBack.setOnClickListener {
+            finish()
+        }
     }
 }
