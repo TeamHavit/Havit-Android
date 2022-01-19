32374125a9498471b79f6df444ada212c61e84a9
안나영 <dpswpfskdud@naver.com>
2022-01-19 19:58:42 +0900

    [FEAT] SaveFragment to ShareActivity (#34)

--- app/src/main/java/org/sopt/havit/ui/save/SaveActivity.kt
+++ app/src/main/java/org/sopt/havit/ui/save/SaveActivity.kt
@@ -1,11 +1,9 @@
 package org.sopt.havit.ui.save
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.havit.R
 import org.sopt.havit.databinding.ActivitySaveBinding
 import org.sopt.havit.ui.base.BaseBindingActivity
-import org.sopt.havit.ui.mypage.MyPageFragment
 
 class SaveActivity : BaseBindingActivity<ActivitySaveBinding>(R.layout.activity_save) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +11,7 @@ class SaveActivity : BaseBindingActivity<ActivitySaveBinding>(R.layout.activity_
         setContentView(binding.root)
 
         //val transaction = supportFragmentManager.beginTransaction()
-        SaveFragment().show(supportFragmentManager,"save")
+        SaveFragment().show(supportFragmentManager, "save")
         /*transaction.add(R.id.fl_save, fragment)
         transaction.commit()*/
 
--- app/src/main/java/org/sopt/havit/ui/save/SaveViewModel.kt
+++ app/src/main/java/org/sopt/havit/ui/save/SaveViewModel.kt
@@ -1,3 +1,15 @@
 package org.sopt.havit.ui.save
 
-class SaveViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SaveViewModel : ViewModel() {
+
+    var _isSaveClick = MutableLiveData<Boolean>()
+    var isSaveClick: LiveData<Boolean> = _isSaveClick
+
+    fun setClick(click: Boolean) {
+        _isSaveClick.value = click
+    }
+}
