f1cd5d35bde358d21f70bdb6e44aa3e0f1696d44
hyein <hyein2072@gmail.com>
2022-01-21 02:44:31 +0900

    [FEAT/UI] Apply Icon (#157)

--- app/src/main/java/org/sopt/havit/ui/category/CategoryContentModifyActivity.kt
+++ app/src/main/java/org/sopt/havit/ui/category/CategoryContentModifyActivity.kt
@@ -8,18 +8,24 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import org.sopt.havit.R
 import org.sopt.havit.databinding.ActivityCategoryContentModifyBinding
 import org.sopt.havit.databinding.ActivityCategoryOrderModifyBinding
 import org.sopt.havit.ui.base.BaseBindingActivity
+import org.sopt.havit.ui.share.IconAdapter
 
 class CategoryContentModifyActivity : BaseBindingActivity<ActivityCategoryContentModifyBinding>(R.layout.activity_category_content_modify) {
     private lateinit var categoryOrderModifyAdapter: CategoryOrderModifyAdapter
     private val categoryContentModifyViewModel: CategoryContentModifyViewModel by viewModels()
     var position = -1
+    private lateinit var iconAdapter : IconAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +33,7 @@ class CategoryContentModifyActivity : BaseBindingActivity<ActivityCategoryConten
         binding.categoryViewModel = categoryContentModifyViewModel
         setContentView(binding.root)
 
+        initAdapter()
         setData()
         clickBack()
         clickDelete()
@@ -89,5 +96,32 @@ class CategoryContentModifyActivity : BaseBindingActivity<ActivityCategoryConten
             setResult(RESULT_FIRST_USER, intentName)
             finish()
         }
+    }
+
+    private fun initAdapter() {
+        iconAdapter = IconAdapter()
+        binding.rvIcon.adapter = iconAdapter
+        iconAdapter.iconList.addAll(
+            listOf(
+                dummyImg,dummyImg2,dummyImg,dummyImg2,dummyImg,dummyImg2,dummyImg,dummyImg2,dummyImg,dummyImg2,
+                dummyImg,dummyImg2,dummyImg,dummyImg2,dummyImg
+            )
+        )
+        iconAdapter.notifyDataSetChanged()
+
+
+        iconAdapter.setItemClickListener(object : IconAdapter.OnItemClickListener{
+            override fun onClick(v: View, position: Int) {
+                Log.d("IconAdapter", "$position clicked in Fragment")
+                v.background = ContextCompat.getDrawable(v.context, R.drawable.oval_gray_stroke_2)
+            }
+        } )
+    }
+
+    companion object {
+        const val dummyImg =
+            "https://user-images.githubusercontent.com/68214704/149118495-e9cc9770-785d-4644-9956-9e17a6641180.png"
+        const val dummyImg2 =
+            "https://user-images.githubusercontent.com/59546818/149669476-3387dddf-d3cb-464d-a1a7-985cc3f397aa.png"
     }
 }
