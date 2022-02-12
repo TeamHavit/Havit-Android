98f0d52c48af6150f57003d6760bb4fd702cd51f
KxxHyoRim <rlagyfla0709@gmail.com>
2022-02-12 19:22:04 +0900

    [UI] 카테고리 추가 시 중복여부에 따른 다음 버튼 활성화 설정 (#267)

--- app/src/main/java/org/sopt/havit/ui/share/AddCategoryFragment.kt
+++ app/src/main/java/org/sopt/havit/ui/share/AddCategoryFragment.kt
@@ -1,13 +1,12 @@
 package org.sopt.havit.ui.share
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -63,22 +62,15 @@ class AddCategoryFragment : Fragment() {
     }
 
     private fun setTextWatcher() {
-        binding.etCategoryTitle.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                setBtnColor()
-                setTextCount()
-                checkDuplicateCategory(p0.toString())
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-            }
-        })
+        binding.etCategoryTitle.addTextChangedListener {
+            setTextCount()
+            if (isTitleNull()) setBtnColor(false)
+            else checkDuplicateCategory(binding.etCategoryTitle.text.toString())
+        }
     }
 
     private fun checkDuplicateCategory(title: String?) {
+        // 중복 된 카테고리가 있는지 검사
         var isDuplicated = false
         for (element in categoryTitleList) {
             if (element == title) {
@@ -86,15 +78,14 @@ class AddCategoryFragment : Fragment() {
                 if (isDuplicated) break
             }
         }
-        if (isDuplicated) {
-            binding.clDuplicateCategory.visibility = View.VISIBLE
-        } else {
-            binding.clDuplicateCategory.visibility = View.INVISIBLE
-        }
+        // 중복 카테고리 여부에 따른 UI 설정(중복 warning & 다음 버튼 색)
+        if (isDuplicated) binding.clDuplicateCategory.visibility = View.VISIBLE
+        else binding.clDuplicateCategory.visibility = View.INVISIBLE
+        setBtnColor(!isDuplicated)
     }
 
-    private fun setBtnColor() {
-        if (isTitleNull()) {
+    private fun setBtnColor(isEnabled: Boolean) {
+        if (!isEnabled) {
             binding.btnNext.setBackgroundColor(resources.getColor(R.color.gray_2))
             binding.btnNext.isEnabled = false
         } else {
