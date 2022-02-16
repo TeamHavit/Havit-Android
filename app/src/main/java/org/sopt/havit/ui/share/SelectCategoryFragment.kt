4abbc7ffde5827f167f7156cfaaeb8366dafee30
KxxHyoRim <rlagyfla0709@gmail.com>
2022-02-16 23:41:04 +0900

    [FEAT] 카테고리 개수 초과 시 카테고리 생성 차단 (#299)

--- app/src/main/java/org/sopt/havit/ui/share/SelectCategoryFragment.kt
+++ app/src/main/java/org/sopt/havit/ui/share/SelectCategoryFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -20,12 +21,13 @@ class SelectCategoryFragment : Fragment() {
     private var _binding: FragmentSelectCategoryBinding? = null
     private val binding get() = _binding!!
     lateinit var categoryData: List<CategoryResponse.AllCategoryData>
-    lateinit var clickCountList : Array<Boolean>
+    private var categoryNum: Int = 0
+    lateinit var clickCountList: Array<Boolean>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentSelectCategoryBinding.inflate(layoutInflater, container, false)
 
         initView()
@@ -40,7 +42,7 @@ class SelectCategoryFragment : Fragment() {
         toolbarClickListener()
     }
 
-    private fun toolbarClickListener(){
+    private fun toolbarClickListener() {
         binding.icClose.setOnClickListener {
             requireActivity().finish()
         }
@@ -53,11 +55,10 @@ class SelectCategoryFragment : Fragment() {
                 // 서버 통신
                 val response =
                     RetrofitObject.provideHavitApi(
-                        MySharedPreference
-                            .getXAuthToken(requireContext())
-                    )
-                        .getCategoryNum()
+                        MySharedPreference.getXAuthToken(requireContext())
+                    ).getCategoryNum()
                 categoryData = response.data
+                categoryNum = categoryData.size
 
                 Log.d("SelectCategoryFragment", categoryData.toString())
                 Log.d("SelectCategoryFragment_len", categoryData.size.toString())
@@ -97,13 +98,13 @@ class SelectCategoryFragment : Fragment() {
         }
     }
 
-    private fun isSelectedLeastOneCategory() : Boolean{
+    private fun isSelectedLeastOneCategory(): Boolean {
 
         val cateSize = clickCountList.size
         Log.d("isSelectedLeastOneCategory_size", cateSize.toString())
         var isMoreThanOne = false
-        for (i in 0 until cateSize){
-            if (clickCountList[i]){
+        for (i in 0 until cateSize) {
+            if (clickCountList[i]) {
                 isMoreThanOne = true
                 Log.d("isSelectedLeastOneCategory_for", "check for statement")
                 break
@@ -116,7 +117,7 @@ class SelectCategoryFragment : Fragment() {
     private fun initListener() {
         // 하단 다음 버튼
         binding.btnNext.setOnClickListener {
-            Log.d ( "getSelectedCategoryNum", getSelectedCategoryNum())
+            Log.d("getSelectedCategoryNum", getSelectedCategoryNum())
             // 카테고리 리스트 스트링으로 변경하여 전송
             findNavController().navigate(
                 SelectCategoryFragmentDirections.actionSelectCategoryFragmentToContentsSummeryFragment(
@@ -125,16 +126,25 @@ class SelectCategoryFragment : Fragment() {
             )
         }
 
-        // 상단 카테고리 추가 버튼
+        // 카테고리 추가 버튼
         binding.ivCategoryAdd.setOnClickListener {
-            findNavController().navigate(R.id.action_selectCategoryFragment_to_addCategoryFragment)
+            if (categoryNum >= MAX_CATEGORY_NUM)       // 카테고리 추가 불가 Toast
+                showCategoryMaxToast()
+            else                                       // 카테고리 추가 뷰로 이동
+                findNavController().navigate(R.id.action_selectCategoryFragment_to_addCategoryFragment)
         }
     }
 
-    private fun getSelectedCategoryNum() : String{
+    private fun showCategoryMaxToast() {
+        val toast = Toast(requireContext())
+        toast.view = layoutInflater.inflate(R.layout.toast_max_category, null)
+        toast.show()
+    }
+
+    private fun getSelectedCategoryNum(): String {
         var selectedCategory = ""
-        for (i in clickCountList.indices){
-            if (clickCountList[i]){
+        for (i in clickCountList.indices) {
+            if (clickCountList[i]) {
                 val cateId = categoryData[i].id
                 selectedCategory += ("$cateId ")
             }
@@ -145,5 +155,9 @@ class SelectCategoryFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private const val MAX_CATEGORY_NUM = 23
     }
 }
