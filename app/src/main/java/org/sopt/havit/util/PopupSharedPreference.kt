f97e782abf30da5ac898b9e81ee57756c95f6a3d
yubinquitous <yb9854@naver.com>
2022-03-12 14:06:20 +0900

    [FEAT] 도달률 팝업 SharedPreference 생성 (#373)

---
+++ app/src/main/java/org/sopt/havit/util/PopupSharedPreference.kt
@@ -0,0 +1,41 @@
+package org.sopt.havit.util
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object PopupSharedPreference {
+    private const val DELETE_POPUP_TIME = "delete_popup_time"
+    private const val IS_POPUP = "is_popup"
+
+    fun setDeletePopupTime(context: Context, time: Long) {
+        val prefs: SharedPreferences =
+            context.getSharedPreferences(DELETE_POPUP_TIME, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = prefs.edit()
+        editor.putLong(
+            DELETE_POPUP_TIME,
+            time
+        ).apply()
+    }
+
+    fun getDeletePopupTime(context: Context): Long {
+        val prefs: SharedPreferences =
+            context.getSharedPreferences(DELETE_POPUP_TIME, Context.MODE_PRIVATE)
+        return prefs.getLong(DELETE_POPUP_TIME, 0)
+    }
+
+    fun setIsPopup(context: Context, isPopup: Boolean) {
+        val prefs: SharedPreferences =
+            context.getSharedPreferences(IS_POPUP, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = prefs.edit()
+        editor.putBoolean(
+            IS_POPUP,
+            isPopup
+        ).apply()
+    }
+
+    fun getIsPopup(context: Context): Boolean {
+        val prefs: SharedPreferences =
+            context.getSharedPreferences(IS_POPUP, Context.MODE_PRIVATE)
+        return prefs.getBoolean(IS_POPUP, true)
+    }
+}
