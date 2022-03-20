1b83c9507fb8a8e4cfd76759f39c470e40d7965f
KxxHyoRim <rlagyfla0709@gmail.com>
2022-03-21 07:05:15 +0900

    [FEAT/REFACTOR] 알림설정 뷰 RadioGroup으로 Refactor (#427)

--- app/src/main/java/org/sopt/havit/ui/share/SetNotificationFragment.kt
+++ app/src/main/java/org/sopt/havit/ui/share/SetNotificationFragment.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import org.sopt.havit.R
 import org.sopt.havit.databinding.FragmentSetNotificationBinding
@@ -18,7 +17,6 @@ import java.util.*
 class SetNotificationFragment :
     BaseBindingFragment<FragmentSetNotificationBinding>(R.layout.fragment_set_notification) {
 
-    private lateinit var btnList: Array<TextView>
     private lateinit var notificationTime: String
 
     override fun onCreateView(
@@ -26,13 +24,22 @@ class SetNotificationFragment :
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
 
-        btnList = arrayOf(
-            binding.btn1h, binding.btn2h, binding.btn3h, binding.btnTomorrow, binding.btnChooseTime
-        )
-
+        initRadioGroupListener()
         initToolbarListener()
 
         return binding.root
+    }
+
+    private fun initRadioGroupListener() {
+        binding.rgNotificationTime.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.btn_1h -> getNotificationTime(ONE_HOUR)
+                R.id.btn_2h -> getNotificationTime(TWO_HOUR)
+                R.id.btn_3h -> getNotificationTime(THREE_HOUR)
+                R.id.btn_tomorrow -> getNotificationTime(TWENTY_FOUR_HOUR)
+                else -> getNotificationTime(ONE_MIN) // DatePicker Dialog 로 변경 예정
+            }
+        }
     }
 
     private fun initToolbarListener() {
@@ -43,24 +50,26 @@ class SetNotificationFragment :
         }
     }
 
-    private fun getNotificationTime(idx: Int): String {
-        val cal = Calendar.getInstance()
-        val df: DateFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss")
-
-        cal.time = Date()
-        Log.d("Before change : ", df.format(cal.time))
-        if (idx == 0) cal.add(Calendar.HOUR, 1)
-        if (idx == 1) cal.add(Calendar.HOUR, 2)
-        if (idx == 2) cal.add(Calendar.HOUR, 3)
-        if (idx == 3) cal.add(Calendar.DATE, 1)
-        if (idx == 4) cal.add(Calendar.MINUTE, 1)
+    private fun getNotificationTime(idx: Int) {
+        val cal = Calendar.getInstance().apply { time = Date() }
+        val df: DateFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.getDefault())
+        when (idx) {
+            ONE_HOUR -> cal.add(Calendar.HOUR, 1)
+            TWO_HOUR -> cal.add(Calendar.HOUR, 2)
+            THREE_HOUR -> cal.add(Calendar.HOUR, 3)
+            TWENTY_FOUR_HOUR -> cal.add(Calendar.DATE, 1)
+            else -> cal.add(Calendar.MINUTE, 1)
+        }
         Log.d("After  change : ", df.format(cal.time))
-
-        return df.format(cal.time)
+        notificationTime = df.format(cal.time)
     }
 
 
     companion object {
-        const val SIZE = 5
+        const val ONE_HOUR = 1
+        const val TWO_HOUR = 2
+        const val THREE_HOUR = 3
+        const val TWENTY_FOUR_HOUR = 24
+        const val ONE_MIN = -1
     }
 }
