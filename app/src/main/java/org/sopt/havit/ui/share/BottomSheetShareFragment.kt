d4ffd0fadab92f2340f32cd205bd612973535a75
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-04-05 02:57:31 +0900

    [REFACTOR] onBackPressed 로직 간소화

--- app/src/main/java/org/sopt/havit/ui/share/BottomSheetShareFragment.kt
+++ app/src/main/java/org/sopt/havit/ui/share/BottomSheetShareFragment.kt
@@ -1,9 +1,7 @@
 package org.sopt.havit.ui.share
 
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -42,17 +40,12 @@ class BottomSheetShareFragment : BottomSheetDialogFragment() {
 
     // 안드로이드 자체 뒤로가기 눌렀을 때 BackPressed 감지
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return BottomSheetDialog(requireContext(), R.style.AppBottomSheetDialogTheme).apply {
-            setOnKeyListener { _: DialogInterface, keyCode: Int, keyEvent: KeyEvent ->
-                if (keyCode == KeyEvent.KEYCODE_BACK && keyEvent.action == KeyEvent.ACTION_UP) {
-
-                    val initialView = R.id.selectCategoryFragment
-                    if (binding.fcvShare.findNavController().currentDestination?.id == initialView)
-                        requireActivity().finish()
-                    binding.fcvShare.findNavController().popBackStack()
-
-                    return@setOnKeyListener true
-                }; return@setOnKeyListener false
+        return object : BottomSheetDialog(requireContext(), R.style.AppBottomSheetDialogTheme) {
+            override fun onBackPressed() {
+                val initialView = R.id.selectCategoryFragment
+                if (binding.fcvShare.findNavController().currentDestination?.id == initialView)
+                    requireActivity().finish()
+                binding.fcvShare.findNavController().popBackStack()
             }
         }
     }
