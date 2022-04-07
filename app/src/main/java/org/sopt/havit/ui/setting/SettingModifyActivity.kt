2bc4d6b6e76a76fbfc0b48ca556f4775685ee0ed
yubinquitous <yb9854@naver.com>
2022-04-07 21:37:36 +0900

    [FEAT] 환경설정-내정보수정 클릭이벤트 (#491)

--- app/src/main/java/org/sopt/havit/ui/setting/SettingModifyActivity.kt
+++ app/src/main/java/org/sopt/havit/ui/setting/SettingModifyActivity.kt
@@ -1,12 +1,23 @@
 package org.sopt.havit.ui.setting
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import org.sopt.havit.R
+import org.sopt.havit.databinding.ActivitySettingModifyBinding
+import org.sopt.havit.ui.base.BaseBindingActivity
 
-class SettingModifyActivity : AppCompatActivity() {
+class SettingModifyActivity :
+    BaseBindingActivity<ActivitySettingModifyBinding>(R.layout.activity_setting_modify) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting_modify)
+        setContentView(binding.root)
+
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        binding.clUnregister.setOnClickListener {
+            startActivity(Intent(this, SettingUnregisterActivity::class.java))
+        }
     }
 }
