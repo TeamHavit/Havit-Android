d14ea20f9e4490db54fda773a540fd6a3ae43d2c
yubinquitous <yb9854@naver.com>
2022-04-07 20:16:52 +0900

    [FEAT] 설정-공지사항,약관,개인정보 뷰 추가  (#491)

---
+++ app/src/main/java/org/sopt/havit/ui/setting/SettingPersonalDataActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.havit.ui.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.havit.R
+
+class SettingPersonalDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_personal_data)
+    }
+}
---
+++ app/src/main/java/org/sopt/havit/ui/setting/SettingPolicyActivity.kt
@@ -0,0 +1,13 @@
+package org.sopt.havit.ui.setting
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.havit.R
+
+class SettingPolicyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_policy)
+    }
+}
