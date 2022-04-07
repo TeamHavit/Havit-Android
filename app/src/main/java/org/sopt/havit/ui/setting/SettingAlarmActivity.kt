8bfab609beaf1097ef5391618c08522bc33e30e3
yubinquitous <yb9854@naver.com>
2022-04-07 19:59:35 +0900

    [FEAT] settingAlarm, settingModify뷰 생성 (#491)

---
+++ app/src/main/java/org/sopt/havit/ui/setting/SettingAlarmActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.havit.ui.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.havit.R
+
+class SettingAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_alarm)
+    }
+}
