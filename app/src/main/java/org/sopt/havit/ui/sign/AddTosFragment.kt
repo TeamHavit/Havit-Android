d618ddc31d41f76838738b511c7cf1095560d2b7
안나영 <dpswpfskdud@naver.com>
2022-04-07 18:33:20 +0900

    [FEAT] AddTosFragment Havit 시작하기 클릭 리스너 구현 (#494)

--- app/src/main/java/org/sopt/havit/ui/sign/AddTosFragment.kt
+++ app/src/main/java/org/sopt/havit/ui/sign/AddTosFragment.kt
@@ -1,9 +1,12 @@
 package org.sopt.havit.ui.sign
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import org.sopt.havit.MainActivity
 import org.sopt.havit.R
 import org.sopt.havit.databinding.FragmentAddTosBinding
 import org.sopt.havit.ui.base.BaseBindingFragment
@@ -11,14 +14,29 @@ import org.sopt.havit.ui.base.BaseBindingFragment
 
 class AddTosFragment : BaseBindingFragment<FragmentAddTosBinding>(R.layout.fragment_add_tos) {
 
+    private val signInViewModel: SignInViewModel by activityViewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
-        // Inflate the layout for this fragment
+        binding.vm = signInViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.btnTosStart.setOnClickListener {
+            activity?.finish()
+            startActivity(Intent(requireContext(), MainActivity::class.java))
+        }
     }
 
 
--- app/src/main/java/org/sopt/havit/ui/sign/SignInViewModel.kt
+++ app/src/main/java/org/sopt/havit/ui/sign/SignInViewModel.kt
@@ -1,5 +1,14 @@
 package org.sopt.havit.ui.sign
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SignInViewModel : ViewModel()
+class SignInViewModel : ViewModel(){
+    private var _isSaveClick = MutableLiveData<Boolean>()
+    var isSaveClick: LiveData<Boolean> = _isSaveClick
+
+    fun setClick(click: Boolean) {
+        _isSaveClick.value = click
+    }
+}
