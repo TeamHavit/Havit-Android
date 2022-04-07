bd47987599a6e50fd9e1bad10ea584416e860dff
yubinquitous <yb9854@naver.com>
2022-04-07 19:59:58 +0900

    [FEAT] 환경설정뷰 userData 서버 연결 (#491)

--- app/src/main/java/org/sopt/havit/ui/setting/SettingActivity.kt
+++ app/src/main/java/org/sopt/havit/ui/setting/SettingActivity.kt
@@ -1,14 +1,27 @@
 package org.sopt.havit.ui.setting
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import org.sopt.havit.R
 import org.sopt.havit.databinding.ActivitySettingBinding
 import org.sopt.havit.ui.base.BaseBindingActivity
 
 class SettingActivity : BaseBindingActivity<ActivitySettingBinding>(R.layout.activity_setting) {
+    private val settingViewModel: SettingViewModel by lazy { SettingViewModel(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.vmSetting = settingViewModel
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setData()
+    }
+
+    private fun setData() {
+        settingViewModel.requestUserInfo()
     }
 }
