fde64bbce64a8c61117e3acc988cc184b3a9c979
yubinquitous <yb9854@naver.com>
2022-04-08 01:41:54 +0900

    [FEAT] 환경설정-회원탈퇴 뷰작업 (#491)

--- app/src/main/java/org/sopt/havit/ui/setting/SettingUnregisterActivity.kt
+++ app/src/main/java/org/sopt/havit/ui/setting/SettingUnregisterActivity.kt
@@ -1,12 +1,26 @@
 package org.sopt.havit.ui.setting
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.havit.R
+import org.sopt.havit.databinding.ActivitySettingUnregisterBinding
+import org.sopt.havit.ui.base.BaseBindingActivity
 
-class SettingUnregisterActivity : AppCompatActivity() {
+class SettingUnregisterActivity :
+    BaseBindingActivity<ActivitySettingUnregisterBinding>(R.layout.activity_setting_unregister) {
+    private val settingViewModel: SettingViewModel by lazy { SettingViewModel(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting_unregister)
+        setContentView(binding.root)
+        binding.vmSetting = settingViewModel
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setData()
+    }
+
+    private fun setData() {
+        settingViewModel.requestUserInfo()
     }
 }
