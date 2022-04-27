dc43ae223b2b4a6f7125e4110457c622e2c2ce31
안나영 <dpswpfskdud@naver.com>
2022-04-28 01:28:46 +0900

    [REFACTOR] MainApplication.kt Hilt 코드 적용 (#519)

--- app/src/main/java/org/sopt/havit/MainApplication.kt
+++ app/src/main/java/org/sopt/havit/MainApplication.kt
@@ -2,65 +2,28 @@ package org.sopt.havit
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.android.viewmodel.dsl.viewModel
-import org.koin.core.context.startKoin
-import org.koin.dsl.module
-import org.sopt.havit.domain.repository.ContentsRepository
-import org.sopt.havit.domain.repository.MyPageRepository
-import org.sopt.havit.domain.repository.SearchRepository
-import org.sopt.havit.data.repository.ContentsRepositoryImpl
-import org.sopt.havit.data.repository.MyPageRepositoryImpl
-import org.sopt.havit.data.repository.SearchRepositoryImpl
-import org.sopt.havit.ui.mypage.MyPageViewModel
-import org.sopt.havit.ui.save.SaveViewModel
-import org.sopt.havit.ui.search.SearchViewModel
-import org.sopt.havit.ui.web.WebViewModel
+import dagger.hilt.android.HiltAndroidApp
 
-class MainApplication :Application() {
+@HiltAndroidApp
+class MainApplication : Application() {
 
-    companion object
-    {
+    companion object {
         var instance: MainApplication? = null
     }
 
-    override fun onCreate()
-    {
+    override fun onCreate() {
         super.onCreate()
-
+        instance = this
         KakaoSdk.init(this, "6069fb41c205cbbf8f213b32eec7f2c8")
-        startKoin {
-            // Koin Android logger
-            androidLogger(if (BuildConfig.DEBUG) org.koin.core.logger.Level.ERROR else org.koin.core.logger.Level.NONE)
-            //inject Android context
-            androidContext(this@MainApplication)
-            // use modules
-            modules(myViewModel, myModule)
-        }
+
     }
 
-    private val myViewModel = module {
-        viewModel { SearchViewModel(get(),get()) }
-        viewModel { MyPageViewModel(get()) }
-        viewModel { WebViewModel(get()) }
-        viewModel { SaveViewModel() }
-    }
-
-    private val myModule = module{
-        single<MyPageRepository> { MyPageRepositoryImpl(get()) }
-        single<SearchRepository> { SearchRepositoryImpl(get()) }
-        single<ContentsRepository>{ ContentsRepositoryImpl(get()) }
-    }
-
-    override fun onTerminate()
-    {
+    override fun onTerminate() {
         super.onTerminate()
         instance = null
     }
 
-    fun getMainApplicationContext(): org.sopt.havit.MainApplication
-    {
+    fun getMainApplicationContext(): MainApplication {
         checkNotNull(instance) { "this application does not inherit com.kakao.MainApplication" }
         return instance!!
     }
