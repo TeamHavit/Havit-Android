4dfa658c78c86beadc83b3b4a6bf4e79b1d7d40e
안나영 <dpswpfskdud@naver.com>
2022-05-02 16:26:30 +0900

    [REFACTOR] MainApplication instance 생성 코드 제거 (#525)

--- app/src/main/java/org/sopt/havit/MainApplication.kt
+++ app/src/main/java/org/sopt/havit/MainApplication.kt
@@ -7,24 +7,11 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class MainApplication : Application() {
 
-    companion object {
-        var instance: MainApplication? = null
-    }
 
     override fun onCreate() {
         super.onCreate()
-        instance = this
         KakaoSdk.init(this, "6069fb41c205cbbf8f213b32eec7f2c8")
 
     }
 
-    override fun onTerminate() {
-        super.onTerminate()
-        instance = null
-    }
-
-    fun getMainApplicationContext(): MainApplication {
-        checkNotNull(instance) { "this application does not inherit com.kakao.MainApplication" }
-        return instance!!
-    }
 }
