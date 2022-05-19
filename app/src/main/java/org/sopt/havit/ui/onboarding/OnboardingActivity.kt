0d96345df22e3037ab1392fcc25a88cab73b4bcd
hyein <hyein2072@gmail.com>
2022-05-19 19:38:37 +0900

    [FEAT] OnboardingActivity 초기 생성 (#563)

---
+++ app/src/main/java/org/sopt/havit/ui/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.havit.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.havit.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
