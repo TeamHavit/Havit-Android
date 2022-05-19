cd04405197efe2372143209faf8549ea22ffcfb6
hyein <hyein2072@gmail.com>
2022-05-19 22:36:59 +0900

    [FEAT/UI] UI 수정 및 adapter 설정 (#563)

--- app/src/main/java/org/sopt/havit/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/org/sopt/havit/ui/onboarding/OnboardingActivity.kt
@@ -1,13 +1,26 @@
 package org.sopt.havit.ui.onboarding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.havit.R
+import org.sopt.havit.databinding.ActivityOnboardingBinding
+import org.sopt.havit.ui.base.BaseBindingActivity
 
-class OnboardingActivity : AppCompatActivity() {
+class OnboardingActivity :
+    BaseBindingActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        setContentView(binding.root)
+
+        setViewPager2Adapter()
+        setIndicator()
+    }
+
+    private fun setViewPager2Adapter() {
+        binding.vpOnboarding.adapter = OnboardingVpAdapter(this)
+    }
+
+    private fun setIndicator() {
+        binding.indicatorOnboarding.setViewPager2(binding.vpOnboarding)
     }
 
     companion object {
