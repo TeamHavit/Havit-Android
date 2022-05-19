97fc123ae7271d7828d5e1a6a795643d55b10a56
hyein <hyein2072@gmail.com>
2022-05-19 23:27:27 +0900

    [FEAT] 마지막 온보딩 페이지 체크하여 위젯 바꾸기 (#563)

--- app/src/main/java/org/sopt/havit/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/org/sopt/havit/ui/onboarding/OnboardingActivity.kt
@@ -1,6 +1,7 @@
 package org.sopt.havit.ui.onboarding
 
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import org.sopt.havit.R
 import org.sopt.havit.databinding.ActivityOnboardingBinding
 import org.sopt.havit.ui.base.BaseBindingActivity
@@ -13,6 +14,7 @@ class OnboardingActivity :
 
         setViewPager2Adapter()
         setIndicator()
+        checkLastOnboardingPage()
     }
 
     private fun setViewPager2Adapter() {
@@ -21,6 +23,16 @@ class OnboardingActivity :
 
     private fun setIndicator() {
         binding.indicatorOnboarding.setViewPager2(binding.vpOnboarding)
+    }
+
+    private fun checkLastOnboardingPage() {
+        binding.vpOnboarding.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.lastOnboarding = (position == 4)
+            }
+        })
     }
 
     companion object {
