2d4394fc72a640cee1c2db9a3973f4774b0c95a6
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-06-09 03:06:17 +0900

    [FEAT] BottomSheetMoreFragment에서 transaction 이벤트 추가 (#594)

--- app/src/main/java/org/sopt/havit/ui/contents/more/BottomSheetMoreFragment.kt
+++ app/src/main/java/org/sopt/havit/ui/contents/more/BottomSheetMoreFragment.kt
@@ -15,6 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.havit.R
 import org.sopt.havit.data.remote.ContentsMoreData
 import org.sopt.havit.databinding.FragmentBottomSheetMoreBinding
+import org.sopt.havit.ui.contents.more.edit_category.EditCategoryFromMoreFragment
 import org.sopt.havit.ui.contents.more.edit_title.EditTitleFromMoreFragment
 import org.sopt.havit.util.OnBackPressedHandler
 import org.sopt.havit.util.getCurrentFragmentOnMore
@@ -60,7 +61,7 @@ class BottomSheetMoreFragment : BottomSheetDialogFragment() {
     private fun setDesignatedFragment() {
         when (viewType) {
             Edit_TITLE -> setFragmentWith<EditTitleFromMoreFragment>()
-            MOVE_CATEGORY -> setFragmentWith<EditTitleFromMoreFragment>() // 수정할거임
+            MOVE_CATEGORY -> setFragmentWith<EditCategoryFromMoreFragment>()
             SET_ALARM -> setFragmentWith<EditTitleFromMoreFragment>() // 수정할거임
         }
     }
