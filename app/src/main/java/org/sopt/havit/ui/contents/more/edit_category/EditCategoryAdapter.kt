2029fbbd482779c80281e4841c9a3ff7f6019e90
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-06-13 03:40:55 +0900

    [ADD] Category RecyclerView 추가 (#594)

---
+++ app/src/main/java/org/sopt/havit/ui/contents/more/edit_category/EditCategoryAdapter.kt
@@ -0,0 +1,44 @@
+package org.sopt.havit.ui.contents.more.edit_category
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.havit.databinding.ItemCategoryToggleBinding
+import org.sopt.havit.domain.entity.Category
+
+class EditCategoryAdapter(private val onCategoryClick: (Int) -> Unit) :
+    ListAdapter<Category, EditCategoryAdapter.CategoryViewHolder>(CategoryDataComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemCategoryToggleBinding.inflate(layoutInflater, parent, false)
+        return CategoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        val category = getItem(position)
+        holder.bind(position, category, onCategoryClick)
+    }
+
+    class CategoryViewHolder(private val binding: ItemCategoryToggleBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(
+            position: Int,
+            category: Category,
+            onCategoryClick: (Int) -> Unit
+        ) {
+            binding.category = category
+            binding.root.setOnClickListener { onCategoryClick(position) }
+        }
+    }
+
+    private class CategoryDataComparator : DiffUtil.ItemCallback<Category>() {
+        override fun areItemsTheSame(oldItem: Category, newItem: Category) =
+            oldItem.isSelected == newItem.isSelected
+
+        override fun areContentsTheSame(oldItem: Category, newItem: Category) =
+            oldItem == newItem
+    }
+}
