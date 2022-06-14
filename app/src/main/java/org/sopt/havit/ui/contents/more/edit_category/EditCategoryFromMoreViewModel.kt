7e8e327ecf9999b8a320f1ac078e7bd93d9c6092
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-06-15 05:30:41 +0900

    [FEAT] EditCategoryFromMoreViewModel.kt originCategoryI 초기화 (#594)

--- app/src/main/java/org/sopt/havit/ui/contents/more/edit_category/EditCategoryFromMoreViewModel.kt
+++ app/src/main/java/org/sopt/havit/ui/contents/more/edit_category/EditCategoryFromMoreViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import org.sopt.havit.data.RetrofitObject
+import org.sopt.havit.data.mapper.CategoryMapper
 import org.sopt.havit.data.remote.ContentsMoreData
 import org.sopt.havit.data.remote.ModifyContentCategoryParams
 import org.sopt.havit.domain.entity.Category
@@ -17,7 +18,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class EditCategoryFromMoreViewModel @Inject constructor(
-    authRepository: AuthRepository
+    authRepository: AuthRepository,
+    private val categoryMapper: CategoryMapper
 ) : ViewModel() {
     val token = authRepository.getAccessToken()
     var contentsId = MutableLiveData<Int>()
@@ -25,11 +27,10 @@ class EditCategoryFromMoreViewModel @Inject constructor(
     var categoryList = MutableLiveData<List<Category>>()
         private set
     var originCategoryId = MutableLiveData<List<Int>>()
-        private set
-    var newCategoryId = MutableLiveData<List<Int>>()
-        private set
-    var isCategorySelectedAtLeastOne = MutableLiveData<Boolean>()
-        private set
+    private var newCategoryId =
+        categoryList.value?.filter { it.isSelected ?: throw IllegalArgumentException() }
+    var isCategorySelectedAtLeastOne = newCategoryId?.size != 0
+
     private val _isNetworkCorrespondenceEnd = MutableLiveData<Event<String>>()
     val isNetworkCorrespondenceEnd: MutableLiveData<Event<String>>
         get() = _isNetworkCorrespondenceEnd
@@ -48,33 +49,34 @@ class EditCategoryFromMoreViewModel @Inject constructor(
 
     fun toggleItemSelected(position: Int) {
         categoryList.value?.let {
-            it[position].isSelected = !it[position].isSelected
+            it[position].isSelected = !(it[position].isSelected)
         }
     }
 
     fun getCategoryListWithSelectedInfo() {
         // TODO 카테고리 목록 가져오는 API임
         //  -> 현재 컨텐츠의 소속 목록을 불러오는 API 배포되면 교체해야함
-        kotlin.runCatching {
-            viewModelScope.launch {
+        viewModelScope.launch {
+            kotlin.runCatching {
                 val response = RetrofitObject.provideHavitApi(token).getAllCategoryList()
-                categoryList.postValue(response.data.toMutableList())
+                categoryList.value = response.data
+                originCategoryId.value =
+                    response.data.filter { !it.isSelected }.map { categoryMapper.toCategoryId(it) }
+                Log.d(TAG, "getCategoryList: ${categoryList.value}")
+                Log.d(TAG, "getCategoryList: ${originCategoryId.value}")
+            }.onSuccess {
+                Log.d(TAG, "getCategoryList: success")
+            }.onFailure {
+                Log.d(TAG, "getCategoryList: failure")
             }
-        }.onSuccess {
-            Log.d(TAG, "getCategoryListWithSelectedInfo: success")
-        }.onFailure {
-            Log.d(TAG, "getCategoryListWithSelectedInfo: failure")
         }
     }
 
     fun patchNewCategoryList() {
         viewModelScope.launch {
             kotlin.runCatching {
-                // TODO : 아래 body는 test 코드임 수정 예정
-                val body = ModifyContentCategoryParams(
-                    requireNotNull(contentsId.value),
-                    listOf(1)
-                )
+                val body = // TODO : 아래 body는 test 코드임 수정 예정
+                    ModifyContentCategoryParams(requireNotNull(contentsId.value), listOf(494, 495))
                 val response = RetrofitObject.provideHavitApi(token).modifyContentCategory(body)
                 Log.d(TAG, "patchNewCategoryList: ${response.message}")
             }.onSuccess {
