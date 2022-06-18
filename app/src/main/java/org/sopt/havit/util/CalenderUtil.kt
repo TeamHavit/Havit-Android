53f0521d7a86c4adf9c3a6254b3b9e758502d0b6
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-06-18 23:18:19 +0900

    [CHORE] 불필요한 코드 제거 (#247)

--- app/src/main/java/org/sopt/havit/util/CalenderUtil.kt
+++ app/src/main/java/org/sopt/havit/util/CalenderUtil.kt
@@ -35,27 +35,4 @@ object CalenderUtil {
             displayedValues.add(String.format("%02d", i))
         minutePicker.displayedValues = displayedValues.toTypedArray()
     }
-
-//    val today: Calendar = Calendar.getInstance().apply { time = Date() }
-//
-//    // 무슨 요일인지 가져오기
-//    private fun getDayOfWeekList(duration: Int = DURATION): Array<String?> { // ex. 월
-//        val dayOfWeekList = arrayOfNulls<String>(duration)
-//        val calList = getCalList()
-//        for (i in 0 until duration)
-//            dayOfWeekList[i] = dayStrMapper[calList[i]?.get(Calendar.DAY_OF_WEEK)]
-//
-//        return dayOfWeekList
-//    }
-//
-//    // 몇년 몇월인지 가져오기
-//    fun getCalList(duration: Int = DURATION): Array<Calendar?> {
-//        val calList = arrayOfNulls<Calendar>(DURATION)
-//        val calendarInstance = Calendar.getInstance().apply { time = Date() }
-//        for (i in 0 until duration) {
-//            calList[i] = calendarInstance
-//            calendarInstance.add(Calendar.DATE, 1)
-//        }
-//        return calList
-//    }
 }
