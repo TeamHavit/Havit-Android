289e3466155ce7a065554738d806e2a4ea4686b5
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-06-23 03:21:05 +0900

    [CHORE] ShareViewModel 코드정리 (#594)

--- app/src/main/java/org/sopt/havit/ui/share/ShareViewModel.kt
+++ app/src/main/java/org/sopt/havit/ui/share/ShareViewModel.kt
@@ -12,28 +12,34 @@ import javax.inject.Inject
 class ShareViewModel @Inject constructor(
     authRepository: AuthRepository
 ) : ViewModel() {
+    /** token */
     val token = authRepository.getAccessToken()
 
+    /** title */
     var originTitle = MutableLiveData<String>()
         private set
     var currTitle = MutableLiveData<String>()
+    fun isTitleModified() = originTitle.value != currTitle.value
+
+    /** notification time */
 
     private var _notificationTime = MutableLiveData<String?>()
     val notificationTime: LiveData<String?>
         get() = _notificationTime
 
-    private var _isTimeDirectlySetFromUser = MutableLiveData<Boolean>()
-    val isTimeDirectlySetFromUser: LiveData<Boolean>
-        get() = _isTimeDirectlySetFromUser
-
     fun setNotificationTime(time: String?) {
         _notificationTime.value = time
     }
+
+    private var _isTimeDirectlySetFromUser = MutableLiveData<Boolean>()
+    val isTimeDirectlySetFromUser: LiveData<Boolean>
+        get() = _isTimeDirectlySetFromUser
 
     fun isTimeDirectlySetFromUser(boolean: Boolean) {
         _isTimeDirectlySetFromUser.value = boolean
     }
 
+    /** server event */
     private val _isNetworkCorrespondenceEnd = MutableLiveData<Event<String>>()
     val isNetworkCorrespondenceEnd: MutableLiveData<Event<String>>
         get() = _isNetworkCorrespondenceEnd
@@ -41,6 +47,4 @@ class ShareViewModel @Inject constructor(
     private fun userClicksOnButton() {
         _isNetworkCorrespondenceEnd.value = Event("Finish Server")
     }
-
-    fun isTitleModified() = originTitle.value != currTitle.value
 }
