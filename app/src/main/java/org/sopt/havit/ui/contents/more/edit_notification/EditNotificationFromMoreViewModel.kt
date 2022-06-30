a11d84085f44173adccfa1ade353932344f0cea2
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-07-01 01:04:50 +0900

    [ADD] EditNotificationFromMoreViewModel.kt (#622)

---
+++ app/src/main/java/org/sopt/havit/ui/contents/more/edit_notification/EditNotificationFromMoreViewModel.kt
@@ -0,0 +1,91 @@
+package org.sopt.havit.ui.contents.more.edit_notification
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.sopt.havit.data.remote.ContentsMoreData
+import org.sopt.havit.domain.repository.AuthRepository
+import org.sopt.havit.ui.share.notification.AfterTime
+import org.sopt.havit.util.CalenderUtil
+import org.sopt.havit.util.Event
+import java.util.*
+import javax.inject.Inject
+
+@HiltViewModel
+class EditNotificationFromMoreViewModel @Inject constructor(
+    authRepository: AuthRepository
+) : ViewModel() {
+    /** token */
+    val token = authRepository.getAccessToken()
+
+    fun initProperty(contentsMoreData: ContentsMoreData) {
+        _finalNotificationTime.value = contentsMoreData.notificationTime
+        if (isNotificationSet()) _finalIndex.value = 4
+    }
+
+    private fun isNotificationSet(): Boolean = finalNotificationTime.value != ""
+
+    /** notification time */
+    private var _finalNotificationTime = MutableLiveData<String?>()
+    val finalNotificationTime: LiveData<String?>
+        get() = _finalNotificationTime
+
+    private var _tempNotificationTime = MutableLiveData<String?>()
+    val tempNotificationTime: LiveData<String?>
+        get() = _tempNotificationTime
+
+    private var _tempIndex = MutableLiveData<Int?>()
+    val tempIndex: LiveData<Int?>
+        get() = _tempIndex
+
+    private var _finalIndex = MutableLiveData<Int?>()
+    val finalIndex: LiveData<Int?>
+        get() = _finalIndex
+
+    fun syncTempDataWithFinalData() {
+        _tempNotificationTime.value = finalNotificationTime.value
+        _tempIndex.value = finalIndex.value
+    }
+
+    fun syncFinalDataWithTempData() {
+        _finalNotificationTime.value = tempNotificationTime.value
+        _finalIndex.value = tempIndex.value
+    }
+
+    fun setNotificationTimeDirectly(time: String?) {
+        _tempNotificationTime.value = time
+        _tempIndex.value = 4
+    }
+
+    fun setNotificationTimeIndirectly(afterTime: AfterTime) {
+        val cal = Calendar.getInstance().apply { time = Date() }
+        cal.add(requireNotNull(afterTime.type), requireNotNull(afterTime.interval))
+        _tempNotificationTime.value = CalenderUtil.dateAndTimeWithDotFormatMD.format(cal.time)
+        _tempIndex.value = afterTime.buttonIndex
+    }
+
+    fun setSelectedIndex(index: Int? = null, afterTime: AfterTime? = null) {
+        _tempIndex.value = index ?: afterTime?.buttonIndex
+    }
+
+    fun isNotificationDataChanged(): Boolean {
+        return if (tempIndex.value == 4 && finalIndex.value == 4)
+            tempNotificationTime.value != finalNotificationTime.value
+        else tempIndex.value != finalIndex.value
+    }
+
+    fun deleteNotification() {
+        _finalIndex.value = null
+        _finalNotificationTime.value = null
+    }
+
+    /** server event */
+    private val _isNetworkCorrespondenceEnd = MutableLiveData<Event<String>>()
+    val isNetworkCorrespondenceEnd: MutableLiveData<Event<String>>
+        get() = _isNetworkCorrespondenceEnd
+
+    private fun userClicksOnButton() {
+        _isNetworkCorrespondenceEnd.value = Event("Finish Server")
+    }
+}
