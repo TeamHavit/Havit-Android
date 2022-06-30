27db933427b57712fca779b1a953394d9017c6dd
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-07-01 03:16:24 +0900

    [FEAT] activityviewmodel을 사용하여 picker에서 설정한 시간 알림설정 뷰에 바로 반영 (#622)

--- app/src/main/java/org/sopt/havit/ui/contents/more/edit_notification/EditNotificationFromMoreFragment.kt
+++ app/src/main/java/org/sopt/havit/ui/contents/more/edit_notification/EditNotificationFromMoreFragment.kt
@@ -3,7 +3,7 @@ package org.sopt.havit.ui.contents.more.edit_notification
 import android.os.Bundle
 import android.os.Parcelable
 import android.view.View
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.havit.R
@@ -21,7 +21,7 @@ import org.sopt.havit.util.OnBackPressedHandler
 class EditNotificationFromMoreFragment :
     BaseBindingFragment<FragmentEditNotificationFromMoreBinding>(R.layout.fragment_edit_notification_from_more),
     OnBackPressedHandler {
-    private val viewModel: EditNotificationFromMoreViewModel by viewModels()
+    private val viewModel: EditNotificationFromMoreViewModel by activityViewModels()
     private lateinit var bottomSheetDialogFragment: BottomSheetDialogFragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
