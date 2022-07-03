0fecadabc28be92d7f254ec8115e7c9fa337ab6b
yubinquitous <yb9854@naver.com>
2022-07-03 16:11:18 +0900

    [UI] 해빗 이용 가이드 뷰 작업 (#630)

---
+++ app/src/main/java/org/sopt/havit/ui/home/ServiceGuideActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.havit.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.havit.R
+
+class ServiceGuideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_service_guide)
+    }
+}
