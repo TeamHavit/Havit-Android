bc1ed94b035c747e86ea210e6a6a2976cd7228c2
hyein <hyein2072@gmail.com>
2022-07-08 01:33:58 +0900

    [FEAT] ContentsRepository 생성 (#637)

--- app/src/main/java/org/sopt/havit/domain/repository/ContentsRepository.kt
+++ app/src/main/java/org/sopt/havit/domain/repository/ContentsRepository.kt
@@ -1,8 +1,23 @@
 package org.sopt.havit.domain.repository
 
+import org.sopt.havit.data.remote.BasicResponse
 import org.sopt.havit.data.remote.ContentsHavitResponse
+import org.sopt.havit.domain.entity.Contents
 
 interface ContentsRepository {
 
     suspend fun isSeen(contentsId: Int): ContentsHavitResponse
+
+    suspend fun getContentsByCategory(
+        categoryId: Int,
+        option: String,
+        filter: String
+    ): List<Contents>
+
+    suspend fun getAllContents(
+        option: String,
+        filter: String
+    ): List<Contents>
+
+    suspend fun deleteContents(contentsId: Int): BasicResponse
 }
