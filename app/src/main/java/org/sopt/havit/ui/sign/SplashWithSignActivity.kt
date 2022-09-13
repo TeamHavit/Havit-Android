d884ac0c41a41ab7e0bd642340edd9783f42a191
안나영 <dpswpfskdud@naver.com>
2022-09-13 13:53:23 +0900

    [FIX] 카카오톡으로 로그인 시도중 발생하는 에러 -> 카카오 계정으로 연결 (#726)

--- app/src/main/java/org/sopt/havit/ui/sign/SplashWithSignActivity.kt
+++ app/src/main/java/org/sopt/havit/ui/sign/SplashWithSignActivity.kt
@@ -7,6 +7,7 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.havit.MainActivity
 import org.sopt.havit.R
@@ -69,6 +70,10 @@ class SplashWithSignActivity :
     private fun initSuccessKakaoLoginOserver() {
         signInViewModel.isSuccessKakaoLogin.observe(this, EventObserver {
             if (it) signInViewModel.getSignIn()
+            else UserApiClient.instance.loginWithKakaoAccount(
+                this,
+                callback = signInViewModel.kakaoLoginCallback
+            )
         })
     }
 
