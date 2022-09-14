1a11f76542887492beba0ab7bf238fa3c069e8fc
kxxhyorim <rlagyfla0709@gmail.com>
2022-09-14 23:28:58 +0900

    [CHORE] 카테고리추가 / 에러뷰 처리 (#695)

--- app/src/main/java/org/sopt/havit/ui/share/add_category/EnterCategoryTitleFragment.kt
+++ app/src/main/java/org/sopt/havit/ui/share/add_category/EnterCategoryTitleFragment.kt
@@ -2,16 +2,19 @@ package org.sopt.havit.ui.share.add_category
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.havit.R
 import org.sopt.havit.databinding.FragmentEnterCategoryTitleBinding
+import org.sopt.havit.domain.model.NetworkStatus
 import org.sopt.havit.ui.base.BaseBindingFragment
 import org.sopt.havit.ui.share.AddCategoryViewModel
 import org.sopt.havit.util.KeyBoardUtil
 import org.sopt.havit.util.KeyBoardUtil.setUpAsSoftKeyboard
+import org.sopt.havit.util.setOnSingleClickListener
 
 @AndroidEntryPoint
 class EnterCategoryTitleFragment :
@@ -21,11 +24,14 @@ class EnterCategoryTitleFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
         getExistingCategoryList()
-        initClickListener()
+        initNormalViewClickListener()
+        initErrorViewClickListener()
         setTextWatcher()
-        setKeyBoardUp()
+        setKeyBoardState()
         toolbarClickListener()
         setUpAsSoftKeyboard(view) // 다음버튼 위/아래 움직이게
     }
@@ -45,7 +51,7 @@ class EnterCategoryTitleFragment :
         binding.isDuplicated = viewModel.isCategoryNameAlreadyExist(currentTitle)
     }
 
-    private fun initClickListener() {
+    private fun initNormalViewClickListener() {
         binding.btnNext.setOnClickListener {
             viewModel.setCategoryTitle(binding.etCategoryTitle.text.toString())
             findNavController().navigate(
@@ -55,15 +61,25 @@ class EnterCategoryTitleFragment :
         binding.ivDeleteText.setOnClickListener { binding.etCategoryTitle.text.clear() }
     }
 
+    private fun initErrorViewClickListener() {
+        with(binding.layoutNetworkError) {
+            ibClose.setOnSingleClickListener { requireActivity().finish() }
+            ivRefresh.setOnSingleClickListener {
+                it.startAnimation(AnimationUtils.loadAnimation(context, R.anim.rotation_refresh))
+                getExistingCategoryList()
+            }
+        }
+    }
+
     private fun toolbarClickListener() {
-        binding.icBack.setOnClickListener { findNavController().popBackStack() }
-        binding.icClose.setOnClickListener { requireActivity().finish() }
+        binding.icBack.setOnSingleClickListener { findNavController().popBackStack() }
+        binding.icClose.setOnSingleClickListener { requireActivity().finish() }
     }
 
-    private fun setKeyBoardUp() =
-        KeyBoardUtil.openKeyBoard(requireContext(), binding.etCategoryTitle)
-
-    companion object {
-        const val TAG = "AddCategoryFragment"
-    }
+    private fun setKeyBoardState() =
+        viewModel.enterCategoryNameViewState.observe(viewLifecycleOwner) {
+            if (it == NetworkStatus.Success())
+                KeyBoardUtil.openKeyBoard(requireContext(), binding.etCategoryTitle)
+            else KeyBoardUtil.hideKeyBoard(requireActivity())
+        }
 }
