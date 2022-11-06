502e073c406b5840bf5068778ae541dfba449cc1
kxxhyorim <rlagyfla0709@gmail.com>
2022-11-06 22:16:01 +0900

    [FIX] HomeFragment Viewpager2 page가 임의로 움직이는 버그수정 (#771)

--- app/src/main/java/org/sopt/havit/ui/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/havit/ui/home/HomeFragment.kt
@@ -56,15 +56,12 @@ class HomeFragment : BaseBindingFragment<FragmentHomeBinding>(R.layout.fragment_
         disableRvSmoothScroll()
         setVpSensitivity()
 
-        return binding.root
-    }
-
-    override fun onStart() {
-        super.onStart()
         categoryDataObserve()   // 카테고리 초기화
         recentContentsDataObserve() // 최근저장 콘텐츠 초기화
         notificationDataObserve()   // 알림 아이콘 초기화
         initReachRate() // 도달률 관련 데이터 초기화
+
+        return binding.root
     }
 
     override fun onResume() {
@@ -171,13 +168,11 @@ class HomeFragment : BaseBindingFragment<FragmentHomeBinding>(R.layout.fragment_
 
     private fun categoryDataObserve() {
         with(homeViewModel) {
-            userData.observe(viewLifecycleOwner) { userData ->
-                categoryData.observe(viewLifecycleOwner) { data ->
-                    if (data.isNotEmpty()) {
-                        val list = setList(data, userData.totalContentNumber)
-                        categoryVpAdapter.updateList(list)
-                        binding.layoutCategory.indicatorCategory.attachTo(binding.layoutCategory.vpCategory)
-                    }
+            categoryData.observe(viewLifecycleOwner) { data ->
+                if (data.isNotEmpty()) {
+                    val list = setList(data, data.size)
+                    categoryVpAdapter.updateList(list)
+                    binding.layoutCategory.indicatorCategory.attachTo(binding.layoutCategory.vpCategory)
                 }
             }
         }
