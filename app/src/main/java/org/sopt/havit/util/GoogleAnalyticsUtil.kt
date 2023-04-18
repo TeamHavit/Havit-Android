afb6a763763d181d66eb4e975af3077987b49cf5
kxxhyorim <rlagyfla0709@gmail.com>
2023-04-18 23:22:06 +0900

    [FEAT] userProperty OS_TYPE, ANDROID (#828)

--- app/src/main/java/org/sopt/havit/util/GoogleAnalyticsUtil.kt
+++ app/src/main/java/org/sopt/havit/util/GoogleAnalyticsUtil.kt
@@ -5,13 +5,53 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.analytics.ktx.logEvent
 import com.google.firebase.ktx.Firebase
-import org.sopt.havit.BuildConfig.IS_PROD
 
 object GoogleAnalyticsUtil {
 
     // property
-    private const val STATUS_INSTALLED = "installed"
-    private const val ANALYTICS_USER_PROP = "app_type"
+    private const val OS_TYPE = "os_type"
+    private const val ANDROID = "android"
+
+    init {
+        Firebase.analytics.setUserProperty(OS_TYPE, ANDROID)
+    }
+
+    fun logScreenEvent(screenName: String) {
+        Firebase.analytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
+        }
+    }
+
+    fun logClickEvent(contentName: String) {
+        Firebase.analytics.logEvent(contentName, null)
+    }
+
+    fun logClickEventWithContentCheck(contentName: String, contentCheck: Boolean) {
+        val params = Bundle().apply {
+            putBoolean(CONTENT_CHECK, contentCheck)
+        }
+        Firebase.analytics.logEvent(contentName, params)
+    }
+
+    fun logScreenDurationTimeEvent(contentName: String, durationTime: Long) {
+        Firebase.analytics.logEvent(contentName) {
+            param(SCREEN_TIME, durationTime)
+        }
+    }
+
+    fun logClickEventWithRecommendedSiteNum(contentName: String, siteNum: Int) {
+        val params = Bundle().apply {
+            putInt(SITE_NUM, siteNum)
+        }
+        Firebase.analytics.logEvent(contentName, params)
+    }
+
+    fun logClickEventOnAddCategory(iconNum: Int) {
+        val params = Bundle().apply {
+            putInt(ICON_NUM, iconNum)
+        }
+        Firebase.analytics.logEvent(CLICK_COMPLETE_SELECT_ICON, params)
+    }
 
     // parameter
     private const val CONTENT_CHECK = "click_content_check"
@@ -33,16 +73,19 @@ object GoogleAnalyticsUtil {
     const val SELECT_ICON = "select_icon"
     const val CLICK_COMPLETE_SELECT_ICON = "click_complete_select_icon"
 
+    // mypage
     const val CLICK_MY_CATEGORY = "click_my_category"
     const val CLICK_MY_CONTENT = "click_my_content"
     const val CLICK_CHECKED_CONTENT = "click_checked_content"
+
+    // webview
     const val CLICK_GO_BACK = "click_go_back"
     const val CLICK_REFRESH = "click_refresh"
     const val CLICK_SHARE = "click_share"
     const val CLICK_CONTENT_CHECK = "click_content_check"
     const val CONTENT_SCREEN_TIME = "content_screen_time"
 
-    // 홈
+    // home
     const val CLICK_MUST_SEE_CONTENT = "click_must_see_content"
     const val CLICK_SEARCH_CONTENT = "click_search_content"
     const val CLICK_WHOLE_CATEGORY = "click_whole_category"
@@ -54,55 +97,4 @@ object GoogleAnalyticsUtil {
     const val GNB_MYPAGE = "gnb_mypage"
     const val GNB_ADD_CONTENT = "gnb_add_content"
 
-    fun logScreenEvent(screenName: String) {
-        if (IS_PROD) {
-            Firebase.analytics.setUserProperty(ANALYTICS_USER_PROP, STATUS_INSTALLED)
-            Firebase.analytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
-                param(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
-            }
-        }
-    }
-
-    fun logClickEvent(contentName: String) {
-        if (IS_PROD) {
-            Firebase.analytics.setUserProperty(ANALYTICS_USER_PROP, STATUS_INSTALLED)
-            Firebase.analytics.logEvent(contentName, null)
-        }
-    }
-
-    fun logClickEventWithContentCheck(contentName: String, contentCheck: Boolean) {
-        if (IS_PROD) {
-            Firebase.analytics.setUserProperty(ANALYTICS_USER_PROP, STATUS_INSTALLED)
-            val params = Bundle().apply {
-                putBoolean(CONTENT_CHECK, contentCheck)
-            }
-            Firebase.analytics.logEvent(contentName, params)
-        }
-    }
-
-    fun logScreenDurationTimeEvent(contentName: String, durationTime: Long) {
-        if (IS_PROD) {
-            Firebase.analytics.setUserProperty(ANALYTICS_USER_PROP, STATUS_INSTALLED)
-            Firebase.analytics.logEvent(contentName) {
-                param(SCREEN_TIME, durationTime)
-            }
-        }
-    }
-
-    fun logClickEventWithRecommendedSiteNum(contentName: String, siteNum: Int) {
-        if (IS_PROD) {
-            Firebase.analytics.setUserProperty(ANALYTICS_USER_PROP, STATUS_INSTALLED)
-            val params = Bundle().apply {
-                putInt(SITE_NUM, siteNum)
-            }
-            Firebase.analytics.logEvent(contentName, params)
-        }
-    }
-
-    fun logClickEventOnAddCategory(iconNum: Int) {
-        val params = Bundle().apply {
-            putInt(ICON_NUM, iconNum)
-        }
-        Firebase.analytics.logEvent(CLICK_COMPLETE_SELECT_ICON, params)
-    }
 }
