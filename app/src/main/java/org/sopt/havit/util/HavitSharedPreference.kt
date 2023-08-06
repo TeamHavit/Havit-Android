690a2688565446afda0cccd6880878f170c05c75
kxxhyorim <rlagyfla0709@gmail.com>
2023-08-07 02:21:37 +0900

    [FIX] sharedPreference / access token 가져오는 key 값 통일 (#888)

--- app/src/main/java/org/sopt/havit/util/HavitSharedPreference.kt
+++ app/src/main/java/org/sopt/havit/util/HavitSharedPreference.kt
@@ -8,16 +8,16 @@ import javax.inject.Inject
 class HavitSharedPreference @Inject constructor(@ApplicationContext private val context: Context) {
 
     companion object {
-        private const val X_AUTH_TOKEN_1 = "TOKEN" // 정아
+        private const val X_AUTH_TOKEN = "ACCESS_TOKEN" // 정아
         private const val FIRST_USER = "FIRST_USER"
     }
 
     fun setXAuthToken(xAuthToken: String) {
         val prefs: SharedPreferences =
-            context.getSharedPreferences(X_AUTH_TOKEN_1, Context.MODE_PRIVATE)
+            context.getSharedPreferences(X_AUTH_TOKEN, Context.MODE_PRIVATE)
         val editor: SharedPreferences.Editor = prefs.edit()
         editor.putString(
-            X_AUTH_TOKEN_1,
+            X_AUTH_TOKEN,
             xAuthToken
         )
         editor.apply()
@@ -25,13 +25,13 @@ class HavitSharedPreference @Inject constructor(@ApplicationContext private val 
 
     fun getXAuthToken(): String {
         val prefs: SharedPreferences =
-            context.getSharedPreferences(X_AUTH_TOKEN_1, Context.MODE_PRIVATE)
-        return prefs.getString(X_AUTH_TOKEN_1, "").toString()
+            context.getSharedPreferences(X_AUTH_TOKEN, Context.MODE_PRIVATE)
+        return prefs.getString(X_AUTH_TOKEN, "").toString()
     }
 
     fun clearXAuthToken() {
         val prefs: SharedPreferences =
-            context.getSharedPreferences(X_AUTH_TOKEN_1, Context.MODE_PRIVATE)
+            context.getSharedPreferences(X_AUTH_TOKEN, Context.MODE_PRIVATE)
         prefs.edit().clear().apply()
     }
 
