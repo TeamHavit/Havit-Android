f3fa669f82fff965ea22b8e99bc98662100219a9
kxxhyorim <rlagyfla0709@gmail.com>
2023-12-02 20:34:14 +0900

    [UI] Implement UI of System Maintenance Screen (#903)

--- app/src/main/java/org/sopt/havit/ui/system_maintenance/SystemMaintenanceActivity.kt
+++ app/src/main/java/org/sopt/havit/ui/system_maintenance/SystemMaintenanceActivity.kt
@@ -1,10 +1,8 @@
 package org.sopt.havit.ui.system_maintenance
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
-import org.sopt.havit.HavitFirebaseMessagingService.Companion.TAG
 import org.sopt.havit.R
 import org.sopt.havit.databinding.ActivitySystemMaintenanceBinding
 import org.sopt.havit.ui.base.BaseBindingActivity
@@ -17,8 +15,9 @@ class SystemMaintenanceActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Log.d(TAG, "onCreate: ${systemMaintenanceViewModel.isSystemMaintenance()}")
-        Log.d(TAG, "onCreate: ${systemMaintenanceViewModel.getSystemMaintenanceMessage()}")
-    }
+        binding.viewModel = systemMaintenanceViewModel
 
+        systemMaintenanceViewModel.fetchIsSystemMaintenance()
+        systemMaintenanceViewModel.fetchSystemMaintenanceMessage()
+    }
 }
