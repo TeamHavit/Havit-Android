44b1208de4a6871e557afca1cb0488f0e9818b9d
kxxhyorim <rlagyfla0709@gmail.com>
2024-03-27 03:45:33 +0900

    [FEAT] Firebase Remote Config에서 강제업데이트 버전 로드 (#905)

--- app/src/main/java/org/sopt/havit/domain/repository/SystemMaintenanceRepository.kt
+++ app/src/main/java/org/sopt/havit/domain/repository/SystemMaintenanceRepository.kt
@@ -1,8 +1,12 @@
 package org.sopt.havit.domain.repository
 
+import kotlinx.coroutines.flow.Flow
+
 interface SystemMaintenanceRepository {
 
     suspend fun isSystemMaintenance(): Boolean
 
     suspend fun getSystemMaintenanceMessage(): String
+
+    fun getForcedUpdateVer(): Flow<Int>
 }
